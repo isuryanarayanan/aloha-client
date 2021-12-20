7d5866d05c4e5565d375335a30e72166967c5080
isuryanarayanan <devdoodan@gmail.com>
2021-12-20 22:51:14 +0530

    redeploy

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import gAuthPlugin from "vue3-google-oauth2";
 var development =
   "1027589441319-hc37u1500vd6p3engsq9pgcllcstfs4l.apps.googleusercontent.com";
 var deployment =
-  "1027589441319-e1vie4dkmdpdit6q8ps3tqpqo0dafj52.apps.googleusercontent.com";
+	"1027589441319-13rlrl6k4hlht32in3a90m8nm3jt9aab.apps.googleusercontent.com";
 
 const gAuthOptions = {
 	clientId: deployment,
